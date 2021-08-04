a2e1fdc146ca00d427b13b2e08b7b7ec1f5baaf5
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-04 21:29:58 +0530

    Added all array practice problems

---
+++ Arrays/RepeatedDigits.js
@@ -0,0 +1,9 @@
+var count = 0;
+
+for ( let index = 0; index <= 100; index++) {
+    if(Math.floor(index/10) == (index % 10)) {
+        digitArray[count] = index;
+        count++;
+    }
+}
+console.log("Repeated Digits are : " + digitArray);
